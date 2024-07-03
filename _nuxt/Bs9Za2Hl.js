ad40eb91a8419936e5502b465bc2a25d91f07c57
empty-wasp <z206674576@163.com>
2024-07-03 22:19:32 +0800

    feat: updsate

---
+++ _nuxt/Bs9Za2Hl.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./BI1uPA13.js";import{I as r,H as t}from"./EBjLtFvi.js";const c={};function o(n,s){return t(),r("div",null,"detector")}const f=e(c,[["render",o]]);export{f as default};
--- i18n.config.ts
+++
@@ -1,8 +0,0 @@
-import en from "./lang/en_us.json";
-import zh from "./lang/zh_cn.json";
-
-export default defineI18nConfig(() => ({
-  legacy: false,
-  locale: "zh",
-  messages: { en, zh },
-}));
