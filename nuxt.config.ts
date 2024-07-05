375f5c12109e50c1fce9f03b7cfc580561c3952e
empty-wasp <z206674576@163.com>
2024-07-05 20:01:24 +0800

    feat: update11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ["@nuxt/ui", "nuxt-swiper", "@nuxtjs/i18n"],
+  app: {
+    baseURL: "/yzbhl/",
+    // buildAssetsDir: "nuxt_assets",
+  },
+  // experimental: {
+  //   payloadExtraction: false,
+  // },
 });
