06e4117caaa9b8729d96cdaee16d04527d360c51
empty-wasp <z206674576@163.com>
2024-07-10 22:39:21 +0800

    feat: update5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,27 +3,18 @@ export default defineNuxtConfig({
   devtools: {
     enabled: false,
   },
+  ssr: false,
+  spaLoadingTemplate: true,
 
-  modules: [
-    "@nuxt/ui",
-    "nuxt-swiper",
-    "@nuxtjs/i18n",
-    "@nuxt/image",
-    "@nuxt/content",
-  ],
+  modules: ["@nuxt/ui", "nuxt-swiper", "@nuxtjs/i18n", "@nuxt/image", "@nuxt/content"],
 
   // "~/assets/css/reset.css",
-  css: ["~/assets/css/main.scss"],
+  css: [ "~/assets/css/main.scss"],
 
   // app: {
   //   baseURL: "/yzbhl/",
   // },
   nitro: {
     compressPublicAssets: true,
-  },
-  content: {
-    experimental: {
-      clientDB: true,
-    },
   },
 });
