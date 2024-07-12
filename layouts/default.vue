5530d4ebbf6d2177521201b101f39dd06e2808de
empty-wasp <z206674576@163.com>
2024-07-12 11:27:27 +0800

    feat: 静态部署测试3

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <AppHeader v-if="result" :menus="result.menus" />
+    <!-- <AppHeader v-if="result" :menus="result.menus" /> -->
     <AppMain><slot /></AppMain>
 
-    <AppFooter v-if="result" :blogroll="result.blogroll" />
+    <!-- <AppFooter v-if="result" :blogroll="result.blogroll" /> -->
 
     <UNotifications />
   </div>
 </template>
 
 <script setup lang="ts">
-const { result }: any = await useAsyncConfig("layout");
+const { data } = await useAsyncData("/", () => queryContent("/").findOne());
+console.log(data);
+
+// const { result }: any = await useAsyncConfig("layout");
 </script>
 
 <style scoped></style>
