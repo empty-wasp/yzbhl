117ef10f5894af98442129b1e5555179d7f89414
empty-wasp <z206674576@163.com>
2024-07-12 11:14:14 +0800

    feat: 静态部署测试2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <!-- <AppHeader v-if="result" :menus="result.menus" /> -->
+    <AppHeader v-if="result" :menus="result.menus" />
     <AppMain><slot /></AppMain>
 
-    <!-- <AppFooter v-if="result" :blogroll="result.blogroll" /> -->
+    <AppFooter v-if="result" :blogroll="result.blogroll" />
 
     <UNotifications />
   </div>
 </template>
 
 <script setup lang="ts">
-// const { result }: any = await useAsyncConfig("layout");
+const { result }: any = await useAsyncConfig("layout");
 </script>
 
 <style scoped></style>
